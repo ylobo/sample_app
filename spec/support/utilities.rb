cc6704b9f82b7e87d8d183759da56d7ad5d2fd2a
ylobo <yule234@gmail.com>
2013-03-09 22:43:34 +0530

    Finish Sign in Sign out

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -7,3 +7,15 @@ include ApplicationHelper
 #    "#{base_title} | #{page_title}" 
 #  end
 #end
+
+def valid_signin(user)
+ fill_in "Email", with: user.email.upcase
+ fill_in "Password", with: user.password 
+ click_button "Sign in"
+end
+
+Rspec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
